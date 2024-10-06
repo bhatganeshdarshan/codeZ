3b12710805322e184f3c856c4bbea680f5b789db
Ganeshdarshan <bhatganeshdarshan10@gmail.com>
2024-10-06 20:13:39 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App : React.FC = () =>{
     return(
         <div>
-            <h1>app componenet</h1>
+            <Navbar></Navbar>
         </div>
     );
 };
